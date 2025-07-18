8d9034a51d8890cfd9bfc4509bca99d63733817a
Agustin Buset <agustin.buset@gmail.com>
2025-07-18 00:55:12 -0300

    Initial commit

---
+++ src/main/java/com/example/MallPLaza/MallPLazaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MallPLaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallPLazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallPLazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MallPLaza/MallPLazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MallPLaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallPLazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
